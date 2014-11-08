d8cb18b2323990a0bc3f8242c4046ea03b2fb9b4
Steven Skoczen <steven@quantumimagery.com>
2014-11-08 13:33:59 -0700

    Fully functional dashboard, though intercom still has data bugs.

--- project/apps/main_site/static/main_site/js/site.js
+++ project/apps/main_site/static/main_site/js/site.js
@@ -28,16 +28,31 @@ buddyupDashboard.controller('mainController', function($scope) {
     $scope.update_display_data = function() {
         $scope.create_combined();
         $scope.create_averaged();
-        if ($scope.sample_type == "raw_data") {
-            $scope.display_dataset = $scope.combined;
-        } else {
+        if ($scope.aggregate) {
             $scope.display_dataset = $scope.averaged;
+        } else {
+            $scope.display_dataset = $scope.combined;
         }
         console.log($scope.display_dataset)
     }
     $scope.create_combined = function() {
         $scope.combined = [];
-        $scope.combined = $scope.combined.concat($scope.data_points)
+        if ($scope.sample_type == "percentages") {
+            var d;
+            for (var d_index in $scope.data_points) {
+                d = angular.copy($scope.data_points[d_index]);
+                d["num_active_users"] = (100 * d["num_active_users"] / d["num_total_users"]).toFixed(0);
+                d["num_authenticated"] = (100 * d["num_authenticated"] / d["num_total_users"]).toFixed(0);
+                d["num_filled_in_profile"] = (100 * d["num_filled_in_profile"] / d["num_total_users"]).toFixed(0);
+                d["num_hit_home_page"] = (100 * d["num_hit_home_page"] / d["num_total_users"]).toFixed(0);
+                d["num_with_one_class"] = (100 * d["num_with_one_class"] / d["num_total_users"]).toFixed(0);
+                d["num_with_one_buddy"] = (100 * d["num_with_one_buddy"] / d["num_total_users"]).toFixed(0);
+                d["num_attended_one_event"] = (100 * d["num_attended_one_event"] / d["num_total_users"]).toFixed(0);
+                $scope.combined.push(d);
+            }
+        } else {
+            $scope.combined = $scope.combined.concat($scope.data_points)
+        }
         var m;
         for (var m_index in $scope.milestones) {
             m = $scope.milestones[m_index];
@@ -82,6 +97,10 @@ buddyupDashboard.controller('mainController', function($scope) {
             console.log("num_samples" + num_samples)
 
             if (m.type == "event" || m.type == "code_push") {
+                buddy_ratio_to_save = buddy_ratio / num_samples.toFixed(0);
+                if ($scope.sample_type == "percentages") {
+                    num_samples = num_samples / 100
+                }
                 if (num_samples > 0) {
                     $scope.averaged.push({
                         "type": "data_point",
@@ -94,21 +113,32 @@ buddyupDashboard.controller('mainController', function($scope) {
                         "num_with_one_class": (num_with_one_class / num_samples).toFixed(0),
                         "num_with_one_buddy": (num_with_one_buddy / num_samples).toFixed(0),
                         "num_attended_one_event": (num_attended_one_event / num_samples).toFixed(0),
-                        "buddy_ratio": buddy_ratio / num_samples.toFixed(0)
+                        "buddy_ratio": buddy_ratio_to_save
                     });
                 }
                 $scope.averaged.push(m);
                 reset_aggregates();
             } else {
                 num_samples += 1;
-                num_total_users += m.num_total_users;
-                num_active_users += m.num_active_users;
-                num_authenticated += m.num_authenticated;
-                num_filled_in_profile += m.num_filled_in_profile;
-                num_hit_home_page += m.num_hit_home_page;
-                num_with_one_class += m.num_with_one_class;
-                num_with_one_buddy += m.num_with_one_buddy;
-                num_attended_one_event += m.num_attended_one_event;
+                if ($scope.sample_type == "percentages") {
+                    num_total_users += m.num_total_users / 100;
+                    num_active_users += m.num_active_users / 100;
+                    num_authenticated += m.num_authenticated / 100;
+                    num_filled_in_profile += m.num_filled_in_profile / 100;
+                    num_hit_home_page += m.num_hit_home_page / 100;
+                    num_with_one_class += m.num_with_one_class / 100;
+                    num_with_one_buddy += m.num_with_one_buddy / 100;
+                    num_attended_one_event += m.num_attended_one_event / 100;
+                } else {
+                    num_total_users += m.num_total_users;
+                    num_active_users += m.num_active_users;
+                    num_authenticated += m.num_authenticated;
+                    num_filled_in_profile += m.num_filled_in_profile;
+                    num_hit_home_page += m.num_hit_home_page;
+                    num_with_one_class += m.num_with_one_class;
+                    num_with_one_buddy += m.num_with_one_buddy;
+                    num_attended_one_event += m.num_attended_one_event;
+                }
                 buddy_ratio += m.buddy_ratio;
             }
         }
--- project/apps/main_site/tasks.py
+++ project/apps/main_site/tasks.py
@@ -14,38 +14,66 @@ SEGMENT_MAPPING = {
     "Hit Home Page": "num_hit_home_page",
     "Logged In": "num_authenticated",
     "Active after Intercom was added": "num_active_users",
+}
 
+# curl https://api.intercom.io/segments -u 5714bb0i:a759c4939f3a832e88e0f9cb5c29ffecf6b032c8 -H 'Accept:application/json'
+SEGMENT_MAPPING_BY_ID = {
+    "544fddd77cdcd41924000003": "num_with_one_class",
+    "544fde65ad77a64760000001": "num_attended_one_event",
+    "544fe5c809b3d12668000016": "num_total_users",
+    "544fe5f43cb3387432000001": "num_filled_in_profile",
+    "544fde2b644dee2065000006": "num_with_one_buddy",
+    "544fdc67e1e6e662f8000037": "num_hit_home_page",
+    "544fdcbef34e7b4f36000072": "num_authenticated",
+    "545b9e6114d10246560000df": "num_active_users",
 }
 
 
 @task(name="get_data")
 def get_data():
+    data = {}
 
     # Get the big counts, etc.
     headers = {'content-type': 'application/json', 'accept': 'application/json'}
-
-    r = requests.get('https://api.intercom.io/counts?type=user&count=segment', data=None, headers=headers, auth=('5714bb0i', settings.INTERCOM_API_KEY))
-    segments = r.json()["user"]["segment"]
-    data = {}
-    for s in segments:
-        for name, value in s.items():
-            if name in SEGMENT_MAPPING:
-                data[SEGMENT_MAPPING[name]] = value
 
     data["recorded_at"] = datetime.datetime.now()
     print "Recording at %s" % data["recorded_at"]
 
     # Get the ratio.
 
-    # Loop through all the ACTIVE (last 6 mo) users, get totay
-    page = 0
-    segment_id = "545b9e6114d10246560000df"
-    url = "https://api.intercom.io/users?segment_id=%s&per_page=50&page=%s" % (segment_id, page)
-
-    end = False
-    num_users = 0
+    # Loop through all the ACTIVE (last 6 mo) users, get total
     num_buddy_requests = 0
     num_buddies = 0
+    per_page = 50
+
+    for key, name in SEGMENT_MAPPING_BY_ID.items():
+        end = False
+        num_users = 0
+        page = 0
+        print name
+        while not end:
+            print page
+
+            url = "https://api.intercom.io/users?segment_id=%s&per_page=%s&page=%s" % (key, per_page, page)
+            r = requests.get(url, data=None, headers=headers, auth=('5714bb0i', settings.INTERCOM_API_KEY))
+            resp = r.json()
+
+            if "next" in resp["pages"] and resp["pages"]["next"]:
+                url = resp["pages"]["next"]
+                num_users += per_page
+                page += 1
+            else:
+                for u in r.json()["users"]:
+                    num_users += 1
+                end = True
+
+        data[name] = num_users
+
+    end = False
+    page = 0
+    num_users = 0
+    segment_id = "545b9e6114d10246560000df"
+    url = "https://api.intercom.io/users?segment_id=%s&per_page=50&page=%s" % (segment_id, page)
 
     while not end:
         r = requests.get(url, data=None, headers=headers, auth=('5714bb0i', settings.INTERCOM_API_KEY))
